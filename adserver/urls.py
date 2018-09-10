4a0f2170875002d737213e86bbfdad55da2f4a41
David Fischer <david@readthedocs.org>
2018-09-10 15:22:18 -0700

    Enable pylint

--- adserver/urls.py
+++ adserver/urls.py
@@ -1,3 +1,5 @@
+"""Ad server URLs"""
+
 from django.conf.urls import url
 
 from .views import dashboard
--- config/settings/base.py
+++ config/settings/base.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.abspath(
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Overridden in Production"
+SECRET_KEY = "Overridden in Production"  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
