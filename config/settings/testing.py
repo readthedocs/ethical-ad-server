d3a1e8520e09a7e5454c49f9e9ef3730f54b7b5c
David Fischer <david@readthedocs.org>
2019-10-18 11:30:34 -0700

    Don't use manifest storage during tests

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,6 +1,11 @@
-from .base import *  # noqa
+from .development import *  # noqa
 
 
 TESTING = True
 TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
 LOGGING["loggers"]["adserver"]["level"] = "ERROR"
+
+# Whitenoise relies on the manifest being present.
+# Which may not be there in testing
+# unless you run `collectstatic` before running tests
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
