1e48ab8a25d4543e7de6f0a610e8d1de452bef08
David Fischer <david@readthedocs.org>
2020-01-20 22:55:04 -0800

    Add an admin action for inviting new users

--- config/settings/base.py
+++ config/settings/base.py
@@ -271,6 +271,7 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
 
 # Celery settings for asynchronous tasks
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html
