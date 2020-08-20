7245d3b336f1f3c4450e8c09cbb804c52dd09e1c
David Fischer <david@readthedocs.org>
2020-08-20 12:20:25 -0700

    Use Django2 style URLs everywhere

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
 """Ethical Ad Server URL Configuration."""
 from django.conf import settings
-from django.conf.urls import include
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import include
+from django.urls import path
 from django.views import defaults as default_views
 
 
@@ -13,22 +13,22 @@ if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(
+        path(
             "400/",
             default_views.bad_request,
             kwargs={"exception": Exception("Bad Request!")},
         ),
-        url(
+        path(
             "403/",
             default_views.permission_denied,
             kwargs={"exception": Exception("Permission Denied")},
         ),
-        url(
+        path(
             "404/",
             default_views.page_not_found,
             kwargs={"exception": Exception("Page not Found")},
         ),
-        url("500/", default_views.server_error),
+        path("500/", default_views.server_error),
     ]
 
     # We can't use `settings.MEDIA_URL` as the pattern since MEDIA_URL may be fully qualified
@@ -38,15 +38,15 @@ if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
 
-        urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [path(r"__debug__/", include(debug_toolbar.urls))] + urlpatterns
 
 if settings.ADSERVER_ADMIN_URL:
     # In development, the Django admin is available at /admin
     # In production, a custom URL path can be specified
     # If no ADSERVER_ADMIN_URL is specified, the Django admin is disabled
-    urlpatterns += [url(r"^{}/".format(settings.ADSERVER_ADMIN_URL), admin.site.urls)]
+    urlpatterns += [path(r"{}/".format(settings.ADSERVER_ADMIN_URL), admin.site.urls)]
 
 urlpatterns += [
-    url(r"^accounts/", include("allauth.urls")),
-    url(r"^", include("adserver.urls")),
+    path(r"accounts/", include("allauth.urls")),
+    path(r"", include("adserver.urls")),
 ]
