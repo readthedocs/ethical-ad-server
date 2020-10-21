2ecabffccb240de8193282ae4dff81374005f7f8
Eric Holscher <eric@ericholscher.com>
2020-10-21 11:10:48 -0700

    Address review feedback

--- adserver/tasks.py
+++ adserver/tasks.py
@@ -1,8 +1,12 @@
 """Celery tasks for the ad server."""
+import datetime
 import logging
 
 from django.db.models import Count
 
+from .constants import CLICKS
+from .constants import IMPRESSION_TYPES
+from .constants import VIEWS
 from .models import GeoImpression
 from .models import Offer
 from .utils import get_ad_day
@@ -12,41 +16,42 @@ log = logging.getLogger(__name__)  # noqa
 
 
 @app.task()
-def daily_update_geos():
-    """Update the Geo index each day."""
-    beginning_of_today = get_ad_day()
-    log.info("Updating GeoImpressions for %s", beginning_of_today)
+def daily_update_geos(day=None):
+    """
+    Update the Geo index each day.
 
-    for values in (
-        Offer.objects.filter(date__gte=beginning_of_today, viewed=True)
-        .values("publisher", "advertisement", "country")
-        .annotate(Count("country"))
-        .filter(country__count__gt=0)
-        .order_by("-country__count")
-    ):
-        impression, _ = GeoImpression.objects.get_or_create(
-            publisher_id=values["publisher"],
-            advertisement_id=values["advertisement"],
-            country=values["country"],
-            date=beginning_of_today,
-        )
-        GeoImpression.objects.filter(pk=impression.pk).update(
-            views=values["country__count"]
+    :arg day: An optional datetime object representing a day
+    """
+    start_date = get_ad_day()
+    if day:
+        start_date = day.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_date = start_date + datetime.timedelta(days=1)
+
+    log.info("Updating GeoImpressions for %s-%s", start_date, end_date)
+
+    for impression_type in IMPRESSION_TYPES:
+        queryset = Offer.objects.filter(
+            date__gte=start_date,
+            date__lt=end_date,  # Must be `lt` and not `lte` or you get the next days data
         )
 
-    for values in (
-        Offer.objects.filter(date__gte=beginning_of_today, clicked=True)
-        .values("publisher", "advertisement", "country")
-        .annotate(Count("country"))
-        .filter(country__count__gt=0)
-        .order_by("-country__count")
-    ):
-        impression, _ = GeoImpression.objects.get_or_create(
-            publisher_id=values["publisher"],
-            advertisement_id=values["advertisement"],
-            country=values["country"],
-            date=beginning_of_today,
-        )
-        GeoImpression.objects.filter(pk=impression.pk).update(
-            clicks=values["country__count"]
-        )
+        if impression_type == CLICKS:
+            queryset = queryset.filter(clicked=True)
+        elif impression_type == VIEWS:
+            queryset = queryset.filter(views=True)
+
+        for values in (
+            queryset.values("publisher", "advertisement", "country")
+            .annotate(Count("country"))
+            .filter(country__count__gt=0)
+            .order_by("-country__count")
+        ):
+            impression, _ = GeoImpression.objects.get_or_create(
+                publisher_id=values["publisher"],
+                advertisement_id=values["advertisement"],
+                country=values["country"],
+                date=start_date,
+            )
+            GeoImpression.objects.filter(pk=impression.pk).update(
+                **{impression_type: values["country__count"]}
+            )
