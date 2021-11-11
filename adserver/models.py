7469b489f52c831bc2b6252ca9f012ef871d5c57
Eric Holscher <eric@ericholscher.com>
2021-11-11 15:00:03 -0800

    Use a setting so we don't need a migration, etc.

--- adserver/models.py
+++ adserver/models.py
@@ -1790,7 +1790,8 @@ class Offer(AdBase):
     class Meta:
         # This is needed because we can't sort on pk to get the created ordering
         ordering = ("-date",)
-        db_table = "adserver_offer_2021_11"
+        if settings.ADSERVER_OFFER_DB_TABLE:
+            db_table = settings.ADSERVER_OFFER_DB_TABLE
 
 
 class PublisherPayout(TimeStampedModel):
