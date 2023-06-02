c1ab78c6f61532c86bb3cc225c5650d5d297dcc7
David Fischer <david@readthedocs.org>
2023-06-02 16:38:48 -0700

    Add calculator features to flight update/renew

--- assets/src/views/flight_update.js
+++ assets/src/views/flight_update.js
@@ -1,11 +1,40 @@
-import * as jquery from 'jquery';
+const ko = require('knockout');
 
 
-if ($('.ea-update-field').length > 0) {
-  $('.ea-update-field').click(function () {
-    var target = $(this).attr("data-target-field");
-    var value = $(this).attr("data-value");
-    $(target).val(value);
-    return false;
-  });
+function FlightUpdateViewModel(method) {
+  this.budget = ko.observable($("#id_budget").val());
+  this.cpm = ko.observable($("#id_cpm").val());
+  this.cpc = ko.observable($("#id_cpc").val());
+  this.sold_impressions = ko.observable($("#id_sold_impressions").val());
+  this.sold_clicks = ko.observable($("#id_sold_clicks").val());
+
+  this.updateBudget = function () {
+    let budget = Math.round(this.budget(), 10);
+
+    if (Number(this.cpm()) > 0) {
+      this.cpc(0);
+      this.sold_clicks(0);
+      this.sold_impressions(parseInt(1000 * budget / Number(this.cpm())));
+    }
+    if (Number(this.cpc()) > 0) {
+      this.cpm(0);
+      this.sold_impressions(0);
+      this.sold_clicks(parseInt(budget / Number(this.cpc())));
+    }
+  }
+
+  this.budget.subscribe(function () {
+    this.updateBudget();
+  }, this);
+  this.cpm.subscribe(function () {
+    this.updateBudget();
+  }, this);
+  this.cpc.subscribe(function () {
+    this.updateBudget();
+  }, this);
+}
+
+
+if (document.querySelectorAll("#flight-renew-form").length > 0 || document.querySelectorAll("#flight-update-form").length > 0) {
+  ko.applyBindings(new FlightUpdateViewModel());
 }
