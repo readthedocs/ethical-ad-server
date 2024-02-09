d920e15a7b3b633cff28f1a865aef4d79990476d
Eric Holscher <eric@ericholscher.com>
2024-02-08 16:53:10 -0800

    More cleanup

--- adserver/analyzer/backends/st.py
+++ adserver/analyzer/backends/st.py
@@ -16,6 +16,9 @@ class SentenceTransformerAnalyzerBackend(BaseAnalyzerBackend):
     Quick and dirty analyzer that uses the SentenceTransformer library
     """
 
+    MODEL_NAME = "multi-qa-MiniLM-L6-cos-v1"
+    MODEL_HOME = os.getenv("SENTENCE_TRANSFORMERS_HOME", "/tmp/sentence_transformers")
+
     def preprocess_text(self, text):
         self.preprocessor = preprocessing.make_pipeline(
             preprocessing.normalize.unicode,
@@ -29,12 +32,7 @@ class SentenceTransformerAnalyzerBackend(BaseAnalyzerBackend):
 
     def embed_response(self, resp) -> list:
         """Analyze an HTTP response and return a list of keywords/topics for the URL."""
-        model = SentenceTransformer(
-            "multi-qa-MiniLM-L6-cos-v1",
-            cache_folder=os.getenv(
-                "SENTENCE_TRANSFORMERS_HOME", "/tmp/sentence_transformers"
-            ),
-        )
+        model = SentenceTransformer(self.MODEL_NAME, cache_folder=self.MODEL_HOME)
 
         soup = BeautifulSoup(resp.content, features="html.parser")
 
