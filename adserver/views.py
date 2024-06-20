e2197c404983a25ae48ad4ff9b6088a1c422aac7
David Fischer <david@readthedocs.org>
2024-06-20 15:29:24 -0700

    Log mismatched browser/os at lower level
    - These happen at pretty low rates in prod
      Browser: ~10/day, OS: ~30/day
    - This change just logs them at a lower level
    - These impressions are not billed both before this change and after

--- adserver/views.py
+++ adserver/views.py
@@ -931,7 +931,7 @@ class BaseProxyView(View):
             reason = "Ratelimited view impression"
         elif offer and offer.os_family != parsed_ua.os.family:
             log.log(
-                self.log_security_level,
+                self.log_level,
                 "Mismatched OS between offer and impression. Publisher: [%s], Offer OS: [%s], User agent: [%s]",
                 offer.publisher,
                 offer.os_family,
@@ -940,7 +940,7 @@ class BaseProxyView(View):
             reason = "Mismatched OS"
         elif offer and offer.browser_family != parsed_ua.browser.family:
             log.log(
-                self.log_security_level,
+                self.log_level,
                 "Mismatched browser between offer and impression. Publisher: [%s], Offer Browser: [%s], User agent: [%s]",
                 offer.publisher,
                 offer.browser_family,
