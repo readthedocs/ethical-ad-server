8537f828724ca16d8bee0da9b5f8bec66c31899f
Eric Holscher <eric@ericholscher.com>
2024-10-01 10:09:57 -0700

    Fix pip-tools to run in CI

--- requirements/update.sh
+++ requirements/update.sh
@@ -3,8 +3,11 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Loop through all .in files in the current directory
-for file in *.in
+# Get the directory where the script is located
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+# Loop through all .in files in the script's directory
+for file in "$SCRIPT_DIR"/*.in
 do
   # Check if the file exists to avoid errors if there are no .in files
   if [[ -f "$file" ]]; then
@@ -12,7 +15,7 @@ do
     # Call pip-compile for each .in file
     pip-compile -U "$file"
   else
-    echo "No .in files found in the directory."
+    echo "No .in files found in $SCRIPT_DIR."
     break
   fi
 done
