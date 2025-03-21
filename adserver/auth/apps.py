2ff2e1b9a7f8c267228c36cefd779f263c941bbf
David Fischer <david@readthedocs.org>
2025-03-21 15:37:23 -0700

    User email verification
    - Mark all existing users verified
    - Mark emails verified on a PW reset/invite

--- adserver/auth/apps.py
+++ adserver/auth/apps.py
@@ -3,7 +3,18 @@
 from django.apps import AppConfig
 
 
+def pw_reset_callback(sender, request, user, **kwargs):
+    """Mark the email verified after a PW reset (includes invite)"""
+    for email in user.emailaddress_set.filter(email=user.email):
+        email.set_verified()
+
+
 class AdServerAuthConfig(AppConfig):
     name = "adserver.auth"
     label = "adserver_auth"
     verbose_name = "Ad Server Auth"
+
+    def ready(self):
+        from allauth.account.signals import password_reset  # noqa
+
+        password_reset.connect(pw_reset_callback)
