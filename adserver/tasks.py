56de7424454ddd6a9d6fb815328c43570a037fa9
David Fischer <david@readthedocs.org>
2025-05-20 11:17:03 -0700

    Use optimized aggregations

--- adserver/tasks.py
+++ adserver/tasks.py
@@ -69,12 +69,26 @@ def daily_update_geos(day=None, geo=True, region=True):
         log.error("geo or region required, please pass one as True")
         return
 
-    # TODO: Delete the GeoImpression, once we're happy with RegionImpression's
-    if geo:
-        log.info("Updating GeoImpressions for %s-%s", start_date, end_date)
+    log.info(
+        "Updating RegionImpressions and/or GeoImpressions for %s-%s",
+        start_date,
+        end_date,
+    )
+
+    if offers_dump_exists(start_date):
+        # Use the optimized aggregation that requires a daily dump of offers to cloud storage
+        from ethicalads_ext.etl.aggregations import GeoAggregation
+        from ethicalads_ext.etl.aggregations import RegionAggregation
+
+        if geo:
+            agg = GeoAggregation(start_date, end_date)
+            agg.aggregate()
+        if region:
+            agg = RegionAggregation(start_date, end_date)
+            agg.aggregate()
+        return
 
     if region:
-        log.info("Updating RegionImpressions for %s-%s", start_date, end_date)
         # Delete all previous Region impressions
         RegionImpression.objects.using("default").filter(
             date__gte=start_date,
@@ -473,6 +487,14 @@ def daily_update_uplift(day=None):
 
     log.info("Updating uplift for %s-%s", start_date, end_date)
 
+    if offers_dump_exists(start_date):
+        # Use the optimized aggregation that requires a daily dump of offers to cloud storage
+        from ethicalads_ext.etl.aggregations import UpliftAggregation
+
+        agg = UpliftAggregation(start_date, end_date)
+        agg.aggregate()
+        return
+
     queryset = Offer.objects.using(settings.REPLICA_SLUG).filter(
         date__gte=start_date,
         date__lt=end_date,  # Things at UTC midnight should count towards tomorrow
@@ -524,9 +546,10 @@ def daily_update_domains(day=None):
 
     if offers_dump_exists(start_date):
         # Use the optimized aggregation that requires a daily dump of offers to cloud storage
-        from ethicalads_ext.etl.aggregations import domains_aggregation
+        from ethicalads_ext.etl.aggregations import DomainAggregation
 
-        domains_aggregation(start_date, end_date)
+        agg = DomainAggregation(start_date, end_date)
+        agg.aggregate()
         return
 
     queryset = Offer.objects.using(settings.REPLICA_SLUG).filter(
@@ -578,6 +601,14 @@ def daily_update_rotations(day=None):
     start_date, end_date = get_day(day)
 
     log.info("Updating rotation data for %s-%s", start_date, end_date)
+
+    if offers_dump_exists(start_date):
+        # Use the optimized aggregation that requires a daily dump of offers to cloud storage
+        from ethicalads_ext.etl.aggregations import RotationAggregation
+
+        agg = RotationAggregation(start_date, end_date)
+        agg.aggregate()
+        return
 
     queryset = Offer.objects.using(settings.REPLICA_SLUG).filter(
         date__gte=start_date,
